ba3bcc635983db1c69eb46f2fbe7ad153548679f
Tri Dao <87549865+Posrabi@users.noreply.github.com>
2021-11-28 00:05:36 -0500

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,11 +1,6 @@
 """
 The first step in the seam carving algorithm: computing the energy of an image.
 
-The functions you fill out in this module will be used as part of the overall
-seam carving process. If you run this module in isolation, the energy of an
-image will be visualized as a grayscale heat map, with brighter spots
-representing pixels:
-
     python3 energy.py surfer.jpg surfer-energy.png
 """
 
@@ -19,13 +14,6 @@ def energy_at(pixels, x, y):
     """
     Compute the energy of the image at the given (x, y) position.
 
-    The energy of the pixel is determined by looking at the pixels surrounding
-    the requested position. In the case the requested position is at the edge
-    of the image, the current position is used whenever a "surrounding position"
-    would go out of bounds.
-
-    This is one of the functions you will need to implement. Expected return
-    value: a single number representing the energy at that point.
     """
     energy = 0
     if y == len(pixels) - 1 or y ==0 or x == len(pixels[0]) - 1 or x == 0:
@@ -45,15 +33,7 @@ def energy_at(pixels, x, y):
 
 def compute_energy(pixels):
     """
-    Compute the energy of the image at every pixel. Should use the `energy_at`
-    function to actually compute the energy at any single position.
-
-    The input is given as a 2D array of colors, and the output should be a 2D
-    array of numbers, each representing the energy value at the corresponding
-    position.
-
-    This is one of the functions you will need to implement. Expected return
-    value: the 2D grid of energy values.
+    Compute the energy level at each individual pixels
     """
     grid = [[None for _ in row] for row in pixels]
     for y in range(len(pixels)):
@@ -72,7 +52,6 @@ def energy_data_to_colors(energy_data):
       2. Convert these values into grayscale colors, where the RGB values are
          all the same for a single color.
 
-    This is NOT one of the functions you have to implement.
     """
 
     colors = [[0 for _ in row] for row in energy_data]
